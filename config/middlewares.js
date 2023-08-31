1e9b4a3e79cefd56dc7b18315408f054b685a61e
NongBritee <berkito_99@hotmail.com>
2023-09-01 02:25:34 +0700

    fix: csp block

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,18 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', 'https://cms-object.maensrischool.ac.th'],
+          'media-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', 'https://cms-object.maensrischool.ac.th']
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
