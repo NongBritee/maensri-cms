be953767638e75f2ff70bdf6be97226161d9f46d
NongBritee <berkito_99@hotmail.com>
2023-10-03 23:57:16 +0700

    add DATABASE_POOL_IDLE_TIMEOUT

--- config/database.js
+++ config/database.js
@@ -25,7 +25,11 @@ module.exports = ({ env }) => {
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: { 
+        min: env.int('DATABASE_POOL_MIN', 2), 
+        max: env.int('DATABASE_POOL_MAX', 10),
+        idleTimeoutMillis: env.int('DATABASE_POOL_IDLE_TIMEOUT', 0),
+      },
     },
   };
 
